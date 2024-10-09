0fbef813f012427e2be403eaf595dfe6d15914b7
abdulsaabir <saabirhaaji10@gmail.com>
2024-10-09 15:47:29 +0100

    Chapter Two

---
+++ Chapter Two/ChapterTwo_task.py
@@ -0,0 +1,26 @@
+# -------------------------------------  EXAMPLE ONE -------------------------------------------------
+# Practice 1: Write a `print()` statement that outputs "Hello, World!" to the console.
+# Practice 2: Write another `print()` statement that outputs "Python is an easy Programming language" to the console.
+
+# -------------------------------------  EXAMPLE TWO -------------------------------------------------
+# Practice 3: Create a variable called `message` and assign the string "Good Morning!" to it.
+# Practice 4: Print the value stored in the `message` variable to the console using `print()`.
+
+# -------------------------------------  EXAMPLE THREE -------------------------------------------------
+# Practice 5: Create a variable called `day` and assign it the string '09' to represent the day.
+# Practice 6: Print the value of `day`, but prevent it from moving to a new line after printing.
+# Practice 7: Create a variable `month` and assign it the value "October". Print `month` with `end='-'` so it stays on the same line.
+# Practice 8: Create a variable `year` and assign it the value `2024`. Print `year` without adding a newline at the end.
+
+# -------------------------------------  EXAMPLE FOUR -------------------------------------------------
+# Practice 9: Create two variables: `age` and `name`. Assign `age` the value `25` and `name` the string `"John"`.
+# Practice 10: Print the values of `age` and `name` in one line, separated by a space.
+
+# -------------------------------------  EXAMPLE FIVE -------------------------------------------------
+# Practice 11: Create three variables: `color`, `animal`, and `fruit`. Assign them the values `"Blue"`, `"Elephant"`, and `"Apple"` respectively.
+# Practice 12: Use `print()` to display a message that includes "The color is:", followed by the value of `color`, 
+# "The animal is:", followed by the value of `animal`, and "The fruit is:", followed by the value of `fruit`.
+
+# -------------------------------------  EXAMPLE SIX -------------------------------------------------
+# Practice 13: Create three variables: `university`, `faculty`, and `id`. Assign `"Jaamacada Ummadda"`, `"Science"`, and `"12345"` to them.
+# Practice 14: Print the value of `university`, followed by `faculty`, and `id`, using newline characters `\n` between them to print each on a separate line.
