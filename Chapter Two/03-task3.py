1cea2fefe40ea7fb977ffdfd26cfbba3491de777
abdulsaabir <saabirhaaji10@gmail.com>
2024-10-23 19:01:00 +0300

    Chapter Two

--- Chapter Two/03-task3.py
+++ Chapter Two/03-task3.py
@@ -1,2 +1,12 @@
-# Define a name and ID
+# -------------------------------------  EXAMPLE ONE -------------------------------------------------
+# Define a name and ID Variables and assign the values 
 # Print both of them in one print using f-string
+
+# -------------------------------------  EXAMPLE TWO -------------------------------------------------
+
+# Ask the user for preferences LIKE favColor, FavAnimal, FavFood etc..
+# Print all information in one print statement using f-string
+
+# -------------------------------------  EXAMPLE THREE -------------------------------------------------
+# Ask the user to input two float numbers
+# Print the division result with four decimal places
