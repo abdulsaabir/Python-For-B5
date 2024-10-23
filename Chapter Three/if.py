a7a665de7e89ed46feeeff83b45e614a522af383
abdulsaabir <saabirhaaji10@gmail.com>
2024-10-23 11:31:11 +0300

    Chapter Three

---
+++ Chapter Three/if.py
@@ -0,0 +1,47 @@
+number =12
+if number > 10:
+    print("this number is bigger then 10")
+
+
+
+
+
+
+
+
+
+# elif number < 10:
+#     print("this number is 10")
+# else:
+#     print("this number is less then 10")
+
+
+# # ask the user to input a random number 
+# # check if that number is positive
+
+
+
+
+RandomNumber = int(input("Enter any number: "))
+if RandomNumber 
+
+
+# if RandomNumber > 0:
+#     print("This is positive Number")
+# else:
+#     print("This is Negative Number")
+# # # if condition: 
+#     # statement
+# # else:
+#     # statement
+
+
+# # ask the user to input what day is today
+# # check if the date is today 
+# # if it's today print today
+# # else print not today 
+# # day = input("Enter any day: ")
+# # if day == "wed":
+# #     print("it's today")
+# # else:
+# #     print("it's not day")
---
+++ Chapter Two/03-task3.py
@@ -0,0 +1,41 @@
+# name = 'hassan'
+# Id = 23
+
+# # print("Your name is:", name, 'and your Id is:', Id)
+# # print(f"Your name is {name} and your Id is {Id}")
+
+# # Ask the user to input some info about himself
+# # print All in one print using F string
+
+# # FavColor = input("Fav color: ")
+# # FavAnimal = input("Fav Animal: ")
+# # FavPlace = input("Fav Place: ")
+# # print(f"Your Fav Color is: {FavColor} \
+# #      Your Fav Animal is: {FavAnimal} \
+# #     And Your Fav Place is {FavPlace}")
+
+
+
+
+# x = float(input("Enter X: "))
+# y = float(input("Enter Y: "))
+# print(f'The result is: {x/y:.4f}')
+
+
+
+
+
+# # print(f'The result is {x/y:.12f}')
+# # # print(f'The sum of {x} and {y} is {x+y}')
+
+
+
+
+
+# # # FavColor = input('Soo geli kalarka aad ugu jeceshahay: ')
+# # # FavAnimal = input("So geli xaywaanka aad ugu jeceshahay: ")
+# # # FavClub = input("Soo geli kooxda aad taageertid: ")
+
+name = ''
+if name:
+    print("Not Empty")
